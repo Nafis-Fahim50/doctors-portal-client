3c8b43431133da2df472e297e27e42415b56f128
Nafis-Fahim50 <nafisf96@gmail.com>
2022-11-14 00:12:34 +0600

    added login form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { FaGoogle } from "react-icons/fa";
 
 const Login = () => {
+    const { register} = useForm();
+    const handleLogin = event =>{
+        event.preventDeafult();
+    }
     return (
-        <div>
-            
+        <div className='h-[600px] flex justify-center items-center'>
+            <div className='w-96 p-7  shadow-xl rounded-xl'>
+                <h2 className='text-xl text-center'>Login</h2>
+                <form onSubmit={handleLogin}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Email</span></label>
+                        <input type="email"
+                            {...register("email", {
+                                required: "Email Address is required"
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Password</span></label>
+                        <input type="password"
+                            {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        <label className="label"> <span className="label-text">Forget Password?</span></label> 
+                    </div>
+                    <input className='btn btn-accent w-full' value="Login" type="submit" />
+                </form>
+                <p>New to Doctors Portal <Link className='text-secondary' to="/signup">Create new Account</Link></p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline text-red-500 w-full'><FaGoogle className='inline mr-2 text-red-500'></FaGoogle>CONTINUE WITH GOOGLE</button>
+                
+            </div>
         </div>
     );
 };
