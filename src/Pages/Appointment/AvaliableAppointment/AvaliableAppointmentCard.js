4856c590bae810d67ef658ec8be705a4e92f73a6
Nafis-Fahim50 <nafisf96@gmail.com>
2022-11-20 14:50:25 +0600

    added price on treatment

--- src/Pages/Appointment/AvaliableAppointment/AvaliableAppointmentCard.js
+++ src/Pages/Appointment/AvaliableAppointment/AvaliableAppointmentCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const AvaliableAppointmentCard = ({ appointment, setTreatment }) => {
-    const { name, slots } = appointment;
+    const { name, slots, price } = appointment;
     return (
         <div className="card shadow-xl">
             <div className="card-body text-center">
                 <h2 className="text-2xl font-bold text-secondary">{name}</h2>
                 <p>{slots.length > 0 ? slots[0] : 'Try Another day'}</p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+                <p className='text-red-500'>Price: ${price}</p>
                 <div className="card-actions justify-center">
                     <label htmlFor="booking-modal"
                    disabled={slots.length === 0}
